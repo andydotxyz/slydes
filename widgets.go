670493542b48b893e822f4f6038964ec07d36a8d
Andy Williams <>
2023-01-28 15:38:24 -0800

    Change slide when tapped

--- widgets.go
+++ widgets.go
@@ -6,9 +6,26 @@ import (
 	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/theme"
+	"fyne.io/fyne/v2/widget"
 
 	"image/color"
 )
+
+type slideButton struct {
+	widget.BaseWidget
+	id      int
+	content fyne.CanvasObject
+	g       *gui
+}
+
+func (s *slideButton) CreateRenderer() fyne.WidgetRenderer {
+	num := fmt.Sprintf("%d:", s.id+1)
+	return widget.NewSimpleRenderer(container.NewHBox(container.NewVBox(canvas.NewText(num, theme.ForegroundColor())), s.content))
+}
+
+func (s *slideButton) Tapped(_ *fyne.PointEvent) {
+	s.g.current.Set(s.id)
+}
 
 func (g *gui) newSlideButton(id int) fyne.CanvasObject {
 	bg := canvas.NewRectangle(color.White)
@@ -24,6 +41,8 @@ func (g *gui) newSlideButton(id int) fyne.CanvasObject {
 	title := canvas.NewText(t, theme.BackgroundColor())
 	title.TextSize = 8
 	slide := newAspectContainer(bg, container.NewPadded(container.NewVBox(title)))
-	num := fmt.Sprintf("%d:", id+1)
-	return container.NewHBox(container.NewVBox(canvas.NewText(num, theme.ForegroundColor())), slide)
+
+	button := &slideButton{id: id, content: slide, g: g}
+	button.ExtendBaseWidget(button)
+	return button
 }
